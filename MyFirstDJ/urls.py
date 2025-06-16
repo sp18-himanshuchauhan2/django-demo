af2b432e5acea42ee594d0d2d4a2dc1b6a99d9b8
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-16 21:17:27 +0530

    update urls.py

--- MyFirstDJ/urls.py
+++ MyFirstDJ/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from myApp import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='myApp'),
-    path('about', views.about, name='about'),
-    path('service', views.service, name='service'),
+    path('admin/', admin.site.urls),
+    path('', include('myApp.urls')),
 ]
--- myApp/urls.py
+++ myApp/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from myApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myApp.urls')),
+    path('', views.index, name='myApp'),
+    path('about', views.about, name='about'),
+    path('service', views.service, name='service'),
 ]
